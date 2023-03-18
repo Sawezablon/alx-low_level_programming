a5ea5f0c9c5c0db0088b653095c45a29f28e3454
zablon <zablonkiprotich16@gmail.com>
2023-03-18 14:41:07 +0000

    edit solution for 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+/**
+  * main - Entry point
+  * Description: use 'putchar' to print all different combination of 2 digits
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		int j;
+
+		for (j = 0 + i; j < 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j + 1);
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
