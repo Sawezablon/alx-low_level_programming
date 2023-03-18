b0c69abbc7fa6b97db86c8400d94c550e14f4e96
zablon <zablonkiprotich16@gmail.com>
2023-03-18 15:43:13 +0000

    Edit solution for 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+/**
+  * main - Entry point
+  * Description: use 'putchar' print possible combinations of 3 digits
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		int j;
+
+		for (j = 0 + i; j < 9; j++)
+		{
+			int k;
+
+			for (k = 1 + j; k < 9; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j + 1);
+				putchar('0' + k + 1);
+				if (i < 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
