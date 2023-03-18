a649b1a41d04e9b83fbb2c36225df751ee6e7b62
zablon <zablonkiprotich16@gmail.com>
2023-03-18 10:26:43 +0000

    Add solution for 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Description: use 'putchar' functio to print single digit number of basw 10
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+
+	return (0);
+}
