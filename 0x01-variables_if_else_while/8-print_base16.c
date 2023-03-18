d4b97baa0487e07c705d3cd23ea44a97ff6da040
zablon <zablonkiprotich16@gmail.com>
2023-03-18 11:43:38 +0000

    edit solution for 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+/**
+  * main - Entry point
+  * Description: use 'putchar' to print all numbers of base 16
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	char c = 'a';
+	int i;
+
+	while (i <= 16)
+	{
+		if (i < 10)
+		{
+			putchar('0' + i);
+		}
+		else if (i > 10)
+		{
+			putchar(c);
+			c++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
