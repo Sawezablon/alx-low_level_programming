012c515498035fea8e3a54e95e2b6f7ebd9ea331
zablon <zablonkiprotich16@gmail.com>
2023-03-19 18:15:45 +0000

    Edit solution for 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
 
+/**
+  * main - Entry block
+  * Description: use 'putchar' print possible combinations of two 2-digit
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		int j;
+
+		for (j = 0; j < 9; j++)
+		{
+			int k;
+
+			for (k = 0; k < 9; k++)
+			{
+				int l;
+
+				for (l = 0; l < 9; l++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + l + 1);
+					if (i < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
