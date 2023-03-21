4dbcf0df558f770fe67b961102c3f755020b5694
Zablon Kiprotich <84498129+Sawezablon@users.noreply.github.com>
2023-03-21 16:29:06 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 /**
-  * main - Entry block
-  * Description: print statement
-  * Return: Always 0
-  */
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
 int main(void)
 {
-	printf("_putchar\n");
+	char ch[] = "_putchar";
+	int i = 0;
+
+	while (ch[i] != '\0')
+	{
+		_putchar(ch[i]);
+		i++;
+	}
+	_putchar('\n');
 
 	return (0);
 }
