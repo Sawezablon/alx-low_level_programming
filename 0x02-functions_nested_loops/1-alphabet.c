cac26b0f975e6e2e40aa4024d7ee552c0e82a8f7
zablon <zablonkiprotich16@gmail.com>
2023-03-21 13:54:25 +0000

    Add solution for 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * main - Entry block
+  * Description: print lowercase using _putchar function
+  * Return: Always 0
+  */
+int main(void)
+{
+	char ch = "a";
+
+	while(ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
