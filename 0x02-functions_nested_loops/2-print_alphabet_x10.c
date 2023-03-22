6a1e09842604de31512e5db6cf5d22671f94cf6f
zablon <zablonkiprotich16@gmail.com>
2023-03-22 07:11:59 +0000

    Add solution for 2-print

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * main - Entry block
+  * Description: use '_putchar' to print alphabet 10 times
+  * Return: Always 0
+  */
+void print_alphabet_x10(void);
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+
+}
