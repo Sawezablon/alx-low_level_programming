d3085a9b356709359f2fccc85e090a8b14ddfbe4
zablon <zablonkiprotich16@gmail.com>
2023-03-23 14:27:34 +0000

    Add file that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * mul - Entry point
+  * @a: parameter
+  * @b: parameter
+  * Description: function that multiplies two integers.
+  * Return: multiplicatio of a and b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
