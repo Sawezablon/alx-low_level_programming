b9a6cda48e7b109a3405e228b49d7f4f7516db53
zablon <zablonkiprotich16@gmail.com>
2023-03-27 22:56:35 +0000

    Add solution that generates random valid passwords for the program

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * _atoi - function that convert a string to an integer.
+  *
+  * @s: parameter
+  * Return: Always 0
+  */
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
+
