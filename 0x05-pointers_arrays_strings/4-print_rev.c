9219deea1d8b211ee7c9f41cca147172c52ba2aa
zablon <zablonkiprotich16@gmail.com>
2023-03-27 15:00:38 +0000

    Add solution for 4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * print_rev - function that prints a string, in reverse
+  *
+  * @s:p\ parameter
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	int i;
+
+	i = strlen(s);
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
