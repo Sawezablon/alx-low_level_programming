6ebebc773203f3d9513e997577c9773906d31100
zablon <zablonkiprotich16@gmail.com>
2023-03-27 13:49:43 +0000

    Add solution for 1-sawap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * swap_int - function that swaps the values of two integers.
+  *
+  * @a: parameter
+  * @b: parameter
+  * Return: void
+  */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
