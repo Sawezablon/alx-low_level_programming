77c5c88c631cef38c9609941b098738dea79c7d8
Zablon Kiprotich <84498129+Sawezablon@users.noreply.github.com>
2023-03-28 01:58:48 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,37 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
-  * _atoi - function that convert a string to an integer.
-  *
-  * @s: parameter
-  * Return: Always 0
-  */
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * Return: first integer found in string
+ */
+
 int _atoi(char *s)
 {
-	return (atoi(s));
-}
+	int i;
+	int r, p;
 
+	r = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
+		{
+			if (r < 0)
+				r = (r * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (p < 0)
+		r *= -1;
+
+	return (r);
+}
