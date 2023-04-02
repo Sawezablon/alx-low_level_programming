94b85fd024e4655cb51419cca00994bbf4c52652
Zablon Kiprotich <84498129+Sawezablon@users.noreply.github.com>
2023-04-02 20:16:49 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,24 @@
 #include "main.h"
-
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
+ * leet - function that encodes a string into 1337.
+ *
+ * @c: Encoded string
+ *
+ * Return: Encoded output
  */
-
-char *leet(char *s)
+char *leet(char *c)
 {
-	int i;
+	int a, b;
+	char ch[] = "aAeEoOtTlL";
+	char ar[] = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (a = 0 ; *(c + a); a++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (b = 0 ; b <= 9 ; b++)
+		{
+			if (ch[b] == c[a])
+				c[a] = ar[b];
+		}
 	}
-
-	return (s);
+	return (c);
 }
