5cc842f0afa86f780aa8868502c88fdc7f6e7e50
zablon <zablonkiprotich16@gmail.com>
2023-04-05 20:18:10 +0000

    Edit solution for 101-wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -20,12 +20,7 @@ int wildcmp(char *s1, char *s2)
 		if (*(s2 + 1) == '\0')
 			return (1);
 
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2 + 1))
-				return (1);
-			s1++;
-		}
+		return wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1);
 	}
 
 	return (0);
