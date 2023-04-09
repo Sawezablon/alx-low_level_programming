5e0db1482844b2d59f933f70003d4c11b09ab818
zablon <zablonkiprotich16@gmail.com>
2023-04-09 12:52:21 +0000

    add file that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  * @argc: count of argument
+  * @argv: array of pointer to a string
+  *
+  * Return: Success(0);
+  */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
