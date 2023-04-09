923369748566a756c9f45b1e7acf25d9057b46e7
zablon <zablonkiprotich16@gmail.com>
2023-04-09 13:37:52 +0000

    Add file that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * main - Entry point
+  * @argc: count argument
+  * @argv: array pointer of a string
+  *
+  * Return: Success(0);
+  */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
