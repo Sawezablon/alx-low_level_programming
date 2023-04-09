b6af2ea94c8e9f0d8fbf3d58e1b89f756216ace5
zablon <zablonkiprotich16@gmail.com>
2023-04-09 13:05:56 +0000

    Edit solution for whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
   * main - Entry point
@@ -10,6 +11,15 @@
   */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
+
+	i = 0;
+	while (argv[0][i] != '\0')
+	{
+		_putchar(argv[0][i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
