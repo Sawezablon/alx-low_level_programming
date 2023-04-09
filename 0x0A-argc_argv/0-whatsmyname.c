12da168d781594cd192bdfc26510b01944176991
zablon <zablonkiprotich16@gmail.com>
2023-04-09 13:30:03 +0000

    Edit solution for myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,19 +11,7 @@
   */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	(void)i;
 	(void)argc;
-	/**
-	  *i = 0;
-	  *while (argv[0][i] != '\0')
-	  *{
-	  *	_putchar(argv[0][i]);
-	  *	i++;
-	  *}
-	  *_putchar('\n');
-	  */
 	printf("%s\n", argv[0]);
 
 	return (0);
