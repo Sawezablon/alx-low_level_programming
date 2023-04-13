15c47c85dfae7fab1d53bf7c90f083ac7861fb7d
zablon <zablonkiprotich16@gmail.com>
2023-04-13 11:31:37 +0000

    Edit solution for malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,11 +4,11 @@
 
 /**
   * malloc_checked - function that allocates memory using malloc.
-  * @b: unsigned int 
+  * @b: unsigned int
   *
   * Return: a pointer to the allocated memory
   */
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
