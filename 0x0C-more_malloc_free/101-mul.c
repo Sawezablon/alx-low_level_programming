a137751d9c72f3b565971789817bbe9ddbcb4475
Zablon Kiprotich <84498129+Sawezablon@users.noreply.github.com>
2023-04-15 02:18:15 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,41 +3,48 @@
 #include "main.h"
 
 /**
- * isDigit - checks if a string contains a non-digit char
- * @str: string to be evaluated
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int isDigit(char *str)
+int is_digit(char *s)
 {
-	for (int i = 0; str[i]; i++)
-		if (str[i] < '0' || str[i] > '9')
-			return (0);
-	return (1);
+    int i = 0;
+
+    while (s[i])
+    {
+        if (s[i] < '0' || s[i] > '9')
+            return (0);
+        i++;
+    }
+    return (1);
 }
 
 /**
- * length - returns the length of a string
- * @str: string to evaluate
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
  * Return: the length of the string
  */
-int length(char *str)
+int _strlen(char *s)
 {
-	int i;
+    int len = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
+    while (s[len] != '\0')
+    {
+        len++;
+    }
+    return (len);
 }
 
 /**
- * handleError - handles errors for main
+ * errors - handles errors for main
  */
-void handleError(void)
+void errors(void)
 {
-	printf("Error\n");
-	exit(98);
+    printf("Error\n");
+    exit(98);
 }
 
 /**
@@ -49,44 +56,44 @@ void handleError(void)
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3 || !isDigit(argv[1]) || !isDigit(argv[2]))
-		handleError();
+    char *s1, *s2;
+    int l1, l2, l, d1, d2, *res, a = 0, c = 0;
 
-	char *num1 = argv[1], *num2 = argv[2];
-	int len1 = length(num1), len2 = length(num2);
-	int len = len1 + len2 + 1, carry, digit1, digit2, *res, a = 0;
-
-	res = calloc(len, sizeof(int));
-	if (!res)
-		return (1);
-
-	for (int i = len1 - 1; i >= 0; i--)
-	{
-		digit1 = num1[i] - '0';
-		carry = 0;
-		for (int j = len2 - 1; j >= 0; j--)
-		{
-			digit2 = num2[j] - '0';
-			carry += res[i + j + 1] + (digit1 * digit2);
-			res[i + j + 1] = carry % 10;
-			carry /= 10;
-		}
-		if (carry > 0)
-			res[i] += carry;
-	}
-
-	for (int i = 0; i < len - 1; i++)
-	{
-		if (res[i])
-			a = 1;
-		if (a)
-			putchar(res[i] + '0');
-	}
-	if (!a)
-		putchar('0');
-	putchar('\n');
-
-	free(res);
-	return (0);
+    s1 = argv[1], s2 = argv[2];
+    if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+        errors();
+    l1 = _strlen(s1);
+    l2 = _strlen(s2);
+    l = l1 + l2 + 1;
+    res = malloc(sizeof(int) * l);
+    if (!res)
+        return (1);
+    for (int i = 0; i <= l1 + l2; i++)
+        res[i] = 0;
+    for (l1 = l1 - 1; l1 >= 0; l1--)
+    {
+        d1 = s1[l1] - '0';
+        c = 0;
+        for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+        {
+            d2 = s2[l2] - '0';
+            c += res[l1 + l2 + 1] + (d1 * d2);
+            res[l1 + l2 + 1] = c % 10;
+            c /= 10;
+        }
+        if (c > 0)
+            res[l1 + l2 + 1] += c;
+    }
+    for (int i = 0; i < l - 1; i++)
+    {
+        if (res[i])
+            a = 1;
+        if (a)
+            putchar(res[i] + '0');
+    }
+    if (!a)
+        putchar('0');
+    putchar('\n');
+    free(res);
+    return (0);
 }
-
