0851d11af17d64459a62e7d93e769949f114620c
zablon <zablonkiprotich16@gmail.com>
2023-04-17 20:10:13 +0000

    Add files

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * init_dog - entry point
+  * @d: parameter
+  * @name: parameter
+  * @age: parameter
+  * @owner: parameter
+  * Return: nothing
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->email = email;
+	d->age = age;
+}
