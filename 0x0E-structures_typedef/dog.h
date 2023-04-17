a81b77a6cae8b44c2a6a9b2423304b233fc00924
zablon <zablonkiprotich16@gmail.com>
2023-04-17 21:19:17 +0000

    edit dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef DOG_H
+ifndef DOG_H
 #define DOG_H
 
 /**
@@ -19,5 +19,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
