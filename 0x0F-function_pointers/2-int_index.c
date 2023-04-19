f10ffc79adec574e18327aab0a9f41bff45bc2db
zablon <zablonkiprotich16@gmail.com>
2023-04-20 01:42:51 +0300

    Add file e thatsearches for an integer.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+  * int_index - Entry point
+  * @array: array of intergers
+  * @size: length of array
+  * @cmp: pointer to a function
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp ==NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
