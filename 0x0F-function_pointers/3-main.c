c397c19ca34d44c3230641a9062d037ae6f260bd
zablon <zablonkiprotich16@gmail.com>
2023-04-20 04:48:42 +0300

    Add files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - Entry point
+  * @argc: argument count
+  * @argv: argument vector
+  *
+  * Return: Success(0);
+  */
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+	char operator;
+	int (*res)(int, int);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = *argv[2];
+	res = get_op_func(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (res == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((operator == '/' || operator == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	calc = res(num1, num2);
+	printf("%d\n", calc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+  * op_add - addition
+  * @a: parameter
+  * @b: parameter
+  * Return: addition
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - subtraction
+  * @a: parameter
+  * @b: parameter
+  * Return: subsraction
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - multiplication
+  * @a: parameter
+  * @b: parameter
+  * Return: multiplication
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - division
+  * @a: parameter
+  * @b: parameter
+  * Return: division
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  * op_mod - mudulus
+  * @a: parameter
+  * @b: parameter
+  * Return: division remainder
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
