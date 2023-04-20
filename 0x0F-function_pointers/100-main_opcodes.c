3272822f08d803d03a28615de005aa2b8ee843fd
zablon <zablonkiprotich16@gmail.com>
2023-04-20 05:12:13 +0300

    add files

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
