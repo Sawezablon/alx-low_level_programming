9aa8bb4996aea81dc0938d87ee3b725c7729d594
Zablon <zablonkiprotich16@gmail.com.com>
2023-05-04 20:36:56 +0300

    Add new files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  *
+  */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i, z;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n == 1)
+		_putchar('1');
+
+	else
+	{
+		i = 1;
+		while (1)
+		{
+			z = i * 2;
+			if (z == n || z + 1 == n)
+				break;
+			else
+				i++;
+		}
+		if (i * 2 == n)
+		{
+			print_binary(i);
+			_putchar('0');
+		}
+		else if ((i * 2) + 1 == n)
+		{
+			print_binary(i);
+			_putchar('1');
+		}
+	}
+}
