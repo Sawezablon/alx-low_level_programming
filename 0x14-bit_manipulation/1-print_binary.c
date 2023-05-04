283af4d772856399c19e71fa1326e092ad6a6ca0
Zablon <zablonkiprotich16@gmail.com.com>
2023-05-04 21:03:12 +0300

    Edit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,14 @@
   */
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp;
-	int shifts;
-
-	if (n == 0)
-		_putchar('0');
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-		;
-	for (; shifts >= 0; shifts--)
+	if (n >> 0)
 	{
-		if ((n >> shifts) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
