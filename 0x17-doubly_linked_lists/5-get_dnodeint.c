a959a3116afc78edccb0d8601688f682b475ca2b
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-16 14:34:41 +0300

    Added

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+*
+*
+*
+*
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int count = 0;
+
+    while(head != NULL)
+    {
+        if(count == index)
+        {
+            break;
+        }
+        else
+        {
+            head = head->next;
+            count++;
+        }
+    }
+    return (head);
+}
