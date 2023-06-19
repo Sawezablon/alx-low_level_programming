17e7254dfbb185a88c3e44726285cd9c2844e9b1
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-19 14:07:23 +0300

    Add 100-operations.so file

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
+#include <stdio.h>
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,5 +24,11 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 #endif /*MAIN_H*/
