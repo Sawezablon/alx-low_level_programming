822718a8cabd32317e73b8f58d7815a777df5a47
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-09 18:31:04 +0300

    Edit 5-hash_table_print.c file

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,7 +2,7 @@
 
 /**
   * hash_table_print - function that prints a hash table.
-  * @ht - is the hash table
+  * @ht: is the hash table
   * Return: Nothing
   */
 void hash_table_print(const hash_table_t *ht)
