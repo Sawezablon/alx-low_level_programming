e33b1b19afed031e2ba7351f1e84d64cea314864
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-09 14:56:23 +0300

    Edit 0-hash_table_create.c file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,8 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
 	table->size = size;
 	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t));
+	if (table->array == NULL)
+		return (NULL);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
